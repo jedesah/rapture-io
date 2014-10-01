8216db33763f706943f328a9693d246ec64cb056
Jon Pretty <jon.pretty@propensive.com>
2014-10-01 15:47:42 +0200

    Updated dependency versions

--- project/project.scala
+++ project/project.scala
@@ -5,10 +5,9 @@ object project extends ProjectSettings {
   def description = "Rapture IO is a general purpose IO library for Scala, providing much of the functionality of java.io and java.net with an idiomatic Scala API"
   
   def dependencies = Seq(
-    "core" -> "0.10.0",
-    "codec" -> "0.10.0",
-    "uri" -> "0.10.0",
-    "mime" -> "0.10.0"
+    "codec" -> "1.0.0",
+    "uri" -> "1.0.0",
+    "mime" -> "0.9.0"
   )
   
   def thirdPartyDependencies = Nil
